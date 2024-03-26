cf96fc0ba61cbe706a914997add091782ec8563b
JustinTo233 <justinto233@gmail.com>
2024-03-26 15:43:21 -0700

    Updated model.js
    -trying model.save()

--- assets/model/model.js
+++ assets/model/model.js
@@ -1,10 +1,18 @@
 import * as tf from "@tensorflow/tfjs";
 
 export const loadModel = async () => {
-  await tf.ready();
-  const model = await tf.loadGraphModel(
-    "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
-  );
-  console.log("Model loaded.");
-  return model;
+  try {
+    await tf.ready();
+    const model = await tf.loadGraphModel(
+      "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
+    );
+    console.log("Model loaded.");
+
+    const saved = await model.save('file:///path/to/my-model');
+
+    console.log("model saved");
+    return saved;
+  } catch (e) {
+    console.log(e);
+  }
 };
