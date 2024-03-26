2580389eae927eb4d541cdf8240170b34e668a39
JustinTo233 <justinto233@gmail.com>
2024-03-26 15:16:58 -0700

    updated model.js

--- assets/model/model.js
+++ assets/model/model.js
@@ -3,7 +3,9 @@ import * as tf from "@tensorflow/tfjs";
 export const loadModel = async () => {
   const modeljson = await require("./model.json");
   await tf.ready();
-  const model = await tf.loadGraphModel(modeljson);
+  const model = await tf.loadGraphModel(
+    "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
+  );
   console.log("Model loaded.");
   return model;
 };
