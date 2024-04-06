9d2e7caed8756cbd29cc0fbe1feea12ce4e859cd
Infiniteskys <146474106+Infiniteskys@users.noreply.github.com>
2024-04-05 17:15:11 -0700

    tst

--- assets/model/model.js
+++ assets/model/model.js
@@ -1,7 +1,5 @@
 import * as tf from "@tensorflow/tfjs";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as FileSystem from "expo-file-system";
-import {bundleResourceIO, decodeJpeg} from '@tensorflow/tfjs-react-native'
+import {asyncStorageIO} from '@tensorflow/tfjs-react-native'
 
 // const modelJSON = require('./test_model/model.json');
 // const weightPaths = [
@@ -23,10 +21,10 @@ export const loadModel = async () => {
     // const model = await tf.loadGraphModel(`${RNFS.DocumentDirectoryPath}/test_model/model.json`);
     // console.log("Model loaded.");
 
-    // // Save model to local storage
-    // const modelPath = `${FileSystem.documentDirectory}/my-model`;
-    // const saved = await model.save(modelPath);
-    // console.log("Model saved to:", modelPath);
+    // Save model to local storage
+    const saved = await model.save(asyncStorageIO('customs'));
+    console.log("Model saved");
+
 
     // Save model path to AsyncStorage
     // const saved  = await AsyncStorage.setItem('custom-model-path', model);
