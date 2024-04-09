129057345114f6564bc18b8349936b123f134fe9
Infiniteskys <146474106+Infiniteskys@users.noreply.github.com>
2024-04-09 16:33:42 -0700

    updated

--- assets/model/model.js
+++ assets/model/model.js
@@ -1,13 +1,5 @@
 import * as tf from "@tensorflow/tfjs";
-import {asyncStorageIO} from '@tensorflow/tfjs-react-native'
-
-// const modelJSON = require('./test_model/model.json');
-// const weightPaths = [
-//   require('./test_model/group1-shard1of4.bin'),
-//   require('./test_model/group1-shard2of4.bin'),
-//   require('./test_model/group1-shard3of4.bin'),
-//   require('./test_model/group1-shard4of4.bin')
-// ];
+import {asyncStorageIO} from "@tensorflow/tfjs-react-native";
 
 export const loadModel = async () => {
   try {
@@ -16,26 +8,16 @@ export const loadModel = async () => {
       "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
     );
     console.log("Model loaded.");
-    console.log(model);
+    model.summary();
 
-    // const model = await tf.loadGraphModel(`${RNFS.DocumentDirectoryPath}/test_model/model.json`);
-    // console.log("Model loaded.");
-
-    // Save model to local storage
+    // const saved = await model.save('file:///path/to/my-model');
     const saved = await model.save(asyncStorageIO('customs'));
-    console.log("Model saved");
+    console.log("model saved");
+    saved.summary();
 
+    const final = await tf.loadLayersModel(saved);
+    console.log(final == model);
 
-    // Save model path to AsyncStorage
-    // const saved  = await AsyncStorage.setItem('custom-model-path', model);
-    // console.log("Model path saved to AsyncStorage.");
-
-    //load model
-    // const saved = await tf.loadGraphModel(bundleResourceIO(modelJSON));
-
-    // await Promise.all(weightPaths.map((path, index) => {
-    //   return tf.loadWeights(bundleResourceIO(null, [path]), model.weights[index]);
-    // }));
 
     return model;
   } catch (e) {
