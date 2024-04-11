c97944edc31b9f85ec1dee425a5ddecf1c5f304c
Infiniteskys <146474106+Infiniteskys@users.noreply.github.com>
2024-04-11 16:19:23 -0700

    123

--- assets/model/model.js
+++ assets/model/model.js
@@ -8,15 +8,12 @@ export const loadModel = async () => {
       "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
     );
     console.log("Model loaded.");
-    model.summary();
 
-    // const saved = await model.save('file:///path/to/my-model');
-    const saved = await model.save(asyncStorageIO('customs'));
-    console.log("model saved");
-    saved.summary();
+    await model.save(asyncStorageIO('customs'));
+    // console.log("model saved");
 
-    const final = await tf.loadLayersModel(saved);
-    console.log(final == model);
+     const final = await tf.loadLayersModel(asyncStorageIO('customs'));
+     console.log(final == model);
 
 
     return model;
