865a2fd14da63dc3593cf257d46d3572287c96f7
Infiniteskys <146474106+Infiniteskys@users.noreply.github.com>
2024-04-11 16:55:32 -0700

    wilson-2

--- assets/model/model.js
+++ assets/model/model.js
@@ -4,13 +4,19 @@ import {asyncStorageIO} from "@tensorflow/tfjs-react-native";
 export const loadModel = async () => {
   try {
     await tf.ready();
-    const model = await tf.loadGraphModel(
-      "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
-    );
+    // const model = await tf.loadGraphModel(
+    //   "https://cs3.calstatela.edu/~cs4962stu01/test_model/model.json"
+    // );
+    // console.log("Model loaded.");
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 5, inputShape: [1]}));
+    model.add(tf.layers.dense({units: 1}));
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
     console.log("Model loaded.");
 
     await model.save(asyncStorageIO('customs'));
-    // console.log("model saved");
+    console.log("model saved");
 
      const final = await tf.loadLayersModel(asyncStorageIO('customs'));
      console.log(final == model);
